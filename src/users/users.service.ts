e7b2dfb2e773c421ad56c957472280b0fd15d06c
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-05-27 23:22:49 +0200

    Editing user.service file by adding orm elements

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+
+  constructor(@InjectRepository(User) private usersRepository: Repository<User>) { }
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.usersRepository.save(this.usersRepository.create(createUserDto));
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(page: number, limit: number): Promise<User[]> {
+    return this.usersRepository.find({
+      take: limit,
+      skip: (page - 1) * limit
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) throw new NotFoundException(`User ${id} not found`);
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: number, updateUserDto: UpdateUserDto): Promise<UpdateResult> {
+    return this.usersRepository.update({ id }, updateUserDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: number): Promise<DeleteResult> {
+    return this.usersRepository.delete({ id });
   }
 }
