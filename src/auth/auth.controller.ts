839c57d2f6e11c23cf82ac3286e8e81aee419091
Epitexam <202238868+epitexam@users.noreply.github.com>
2025-05-28 18:44:20 +0200

    Adding logic in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin-auth.dto';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
@@ -13,14 +13,26 @@ export class AuthController {
 
     @HttpCode(HttpStatus.CREATED)
     @Post('signup')
-    create(@Body() createUserDto: CreateUserDto) {
+    async create(@Body() createUserDto: CreateUserDto) {
+        const userByEmail = await this.usersService.findOneByEmail(createUserDto.email);
+
+        if (userByEmail) { throw new HttpException("This email is already used.", HttpStatus.CONFLICT) }
+
+        const userByUsername = await this.usersService.findOneByUsername(createUserDto.username);
+
+        if (userByUsername) { throw new HttpException("This username is already used.", HttpStatus.CONFLICT) }
+
         return this.usersService.create(createUserDto);
     }
 
 
     @HttpCode(HttpStatus.OK)
     @Post('signin')
-    signin(@Body() signinDto: SigninDto) {
+    async signin(@Body() signinDto: SigninDto) {
+        const userByEmail = await this.usersService.findOneByEmail(signinDto.email);
+
+        if (!userByEmail) { throw new HttpException("No account linked to this email.", HttpStatus.NOT_FOUND) }
+
         return this.authService.signIn(signinDto.email, signinDto.password);
     }
 }
